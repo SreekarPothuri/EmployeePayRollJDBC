5888048d058327c826026fdb73ea8662910c0094
Your Name <you@example.com>
2020-11-20 22:18:59 +0530

    Added Test case to retrieve employees joined btw given date range

--- src/test/java/com/blz/payrolljdbc/EmployeePayRollServiceTest.java
+++ src/test/java/com/blz/payrolljdbc/EmployeePayRollServiceTest.java
@@ -1,5 +1,6 @@
 package com.blz.payrolljdbc;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.Assert;
@@ -28,5 +29,15 @@ public class EmployeePayRollServiceTest {
 		employeePayrollService.updateEmployeeSalary("Terisa", 300000.00);
 		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
 		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenDateRange_WhenRetrieved_ShouldmatchEmployeeCount() throws EmployeePayrollException {
+		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		LocalDate startDate = LocalDate.of(2018, 01, 01);
+		LocalDate endDate = LocalDate.now();
+		List<EmployeePayRollData> employeePayrollData = employeePayrollService
+				.readEmployeePayrollForDateRange(IOService.DB_IO, startDate, endDate);
+		Assert.assertEquals(2, employeePayrollData.size());
 	}
 }

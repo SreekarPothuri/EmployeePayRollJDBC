986855fa05eea72c65fe0d45fd3c213eebbae599
Your Name <you@example.com>
2020-11-20 20:59:19 +0530

    Added Test case to update salary of employee and retrieve table

--- src/test/java/com/blz/payrolljdbc/EmployeePayRollServiceTest.java
+++ src/test/java/com/blz/payrolljdbc/EmployeePayRollServiceTest.java
@@ -11,15 +11,22 @@ import com.blz.payrolljdbc.EmployeePayRollService.IOService;
 public class EmployeePayRollServiceTest {
 
 	static EmployeePayRollService employeePayrollService;
-	
+
 	@BeforeClass
 	public static void EmpPayRollServiceObj() {
 		employeePayrollService = new EmployeePayRollService();
 	}
-	
+
 	@Test
-	public void givenEmployeePayRollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+	public void givenEmployeePayRollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
 		List<EmployeePayRollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
 		Assert.assertEquals(3, employeePayrollData.size());
+	}
+
+	@Test
+	public void givenNewSalaryForEmployee_WhenUpdated_ShouldMatch() throws EmployeePayrollException {
+		employeePayrollService.updateEmployeeSalary("Terisa", 300000.00);
+		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+		Assert.assertTrue(result);
 	}
 }
